e14d2b9d186a8c6389f03565627ac2606598c258
Deni Mazzei <dconkell@gmail.com>
2021-12-30 23:37:48 -0600

    Updates

---
+++ precache-manifest.689e57d1e842c852929ee49b4ca0d9ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806ec13c71c9f0ff7411b088658582f8",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "c5324824c6493e94f537",
+    "url": "/client/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "d019ca754ebbaafa52e9",
+    "url": "/client/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "c5324824c6493e94f537",
+    "url": "/client/static/js/2.10eaa1e1.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/client/static/js/2.10eaa1e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d019ca754ebbaafa52e9",
+    "url": "/client/static/js/main.8da87473.chunk.js"
+  },
+  {
+    "revision": "d64b09180f5230249a46",
+    "url": "/client/static/js/runtime-main.1d4c076d.js"
+  }
+]);
