5698f1c9095d1aa2c38dde8aee146072980c7cd0
Deni Mazzei <dconkell@gmail.com>
2022-01-03 23:27:28 -0600

    debugging Heroku deployment issue

--- server/server.js
+++ server/server.js
@@ -9,8 +9,6 @@ const { authMiddleware } = require("./utils/auth");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(routes);
-
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -26,6 +24,12 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+app.use(routes);
 
 db.once("open", () => {
   app.listen(PORT, () => {
